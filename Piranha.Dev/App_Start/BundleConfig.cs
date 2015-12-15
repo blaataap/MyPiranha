75fdf618c02052a4f26572268cabec0df97e6d36
rick.postmus@gmail.com <rick.postmus@gmail.com>
2015-12-15 16:20:28 +0100

    Initieel

---
+++ Piranha.Dev/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Piranha.Dev.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // CSS style (bootstrap/inspinia)
+            bundles.Add(new StyleBundle("~/Content/bootstrap-css").Include(
+                        "~/Content/bootstrap/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/style.min.css"));
+
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-2.1.4.min.js"));
+
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
+        }
+    }
+}
---
+++ Piranha.Dev/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using Piranha.Dev.App_Start;
+using Piranha.WebPages;
+
+
+namespace Piranha.Dev
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Hooks.Menu.RenderLevelStart += (ui, str, page) =>
+            {
+                str.Append("<ul class=\"nav navbar-nav\">");
+            };
+        }
+    }
+}
